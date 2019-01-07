99aa568b0653f0ce9e6c46f4f8fb36580c75d45b
Ronan Doherty <ronan.odochartaigh@gmail.com>
2019-01-07 11:44:21 +0000

    Updated dependencies

--- src/Services/EmailSenderService.cs
+++ src/Services/EmailSenderService.cs
@@ -2,6 +2,7 @@ using System;
 using System.Net.Mail;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Options;
+using Ansa.Extensions;
 using Schematic.Core;
 
 namespace Schematic.BaseInfrastructure
